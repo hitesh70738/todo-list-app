d272b75fae2778a845602c95d3d9fabbb9532d4c
hitesh70738 <hitesh70738@gmail.com>
2020-12-08 22:01:29 +0000

    comp day 1

--- application/__init__.py
+++ application/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URL"] = "mysql+pymysql://root:1234@34.105.208.208/todolsit"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:1234@34.105.208.208/todolsit"
 
 db = SQLAlchemy(app)
 
