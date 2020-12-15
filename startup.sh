d94dce4e0d13eab0c5e97865577d4aabf2f576f7
hitesh70738 <hitesh70738@gmail.com>
2020-12-15 15:14:29 +0000

    startup

--- startup.sh
+++ startup.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-export DATABSE_URI="mysql+pymysql://root:1234@34.105.208.208/todolsit"
-export SECRET_KEY="AOSEDUIRFGHLIUADSBV"
 cd /opt/todo-list-app
 sudo python3 -m venv venv
 source venv/bin/activate
