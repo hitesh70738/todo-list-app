41a4acf9725ff98c360fc084d6b22615ff2b51bf
hitesh70738 <hitesh70738@gmail.com>
2020-12-16 10:41:07 +0000

    updated

--- startup.sh
+++ startup.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
-cd /opt/todo-list-app
+#!/bin/bash 
+cd /opt/to-do-list
 sudo mkdir /flask_project
 sudo chown -R /flask_project
 sudo python3 -m venv venv
 source venv/bin/activate
-pip3 install -r requiremnets.txt
-python3 app.py 
+pip3 install -r requirements.txt
+python3 app.py
